23107443d82a2251f6a7311e39ae4e6a0189bd43
Jason Livesay <runvnc@gmail.com>
2023-10-14 09:01:52 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 use rhai::{Engine, EvalAltResult};
 use std::fs;
 
-pub fn main() -> Result<(), Box<EvalAltResult>>
-//                          ^^^^^^^^^^^^^^^^^^
-//                          Rhai API error type
-{
-    // Create an 'Engine'
-    let engine = Engine::new();
-    let script = fs::read_to_string("test.rhai").expect("Unable to read the Rhai script file.");
-
-    // Your first Rhai Script
-    //let script = "print(40 + 2);";
-
-    // Run the script - prints "42"
-    let result = engine.eval::<String>(&script)?;
-    
-    println!("Answer {result}");
-
-    // Done!
-    Ok(())
+struct ScriptRunner {
+    engine: Engine,
 }
+
+impl ScriptRunner {
+    fn new() -> Self {
+        Self {
+            engine: Engine::new(),
+        }
+    }
+
+    fn run(&self, fname: &str) -> Result<String, Box<EvalAltResult>> {
+        let script = fs::read_to_string(fname)
+            .expect("Unable to read the Rhai script file.");
+
+        self.engine.eval::<String>(&script)
+    }
+}
+
+fn main() {
+    let runner = ScriptRunner::new();
+    
+    match runner.run("test.rhai") {
+        Ok(result) => println!("Answer: {}", result),
+        Err(e) => eprintln!("Error: {:?}", e),
+    }
+}
+
