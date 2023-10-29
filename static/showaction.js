18bce3ecf0bcf22afb3cdb93198192f066d88957
Jason Livesay <runvnc@gmail.com>
2023-10-29 17:59:07 +0000

    update

--- static/showaction.js
+++ static/showaction.js
@@ -1,13 +1,13 @@
 function showFunctionCall(funcName, params, result) {
   let paramshtml = ''
   for (var key in params) {
-      paramshtml += `<span class="text-yellow-500">${key}> `;
-      paramshtml += `<span class="text-cyan-500">${params[key]}>  `;
+      paramshtml += `<span class="param_name">${key}</span> `;
+      paramshtml += `<span class="param_value">${params[key]}</span>  `;
   }
 
   let html = ` 
-    <div class="border-2 border-blue-500 p-4 m-2 rounded-lg bg-gray-800"  >
-      ⚡  <span class="text-orange-500">${funcName}</span> ${paramshtml} 
+    <div class="block border-2 border-blue-500 p-4 m-2 rounded-lg bg-gray-800"  >
+      ⚡  <span class="fn_name">${funcName}</span> ${paramshtml} 
       <details class="block">
         <summary>Result</summary>
         <pre><code class="font-mono whitespace-pre-wrap" >${result}</code></pre>
