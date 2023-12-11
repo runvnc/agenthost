498dc92fddeee85a6bcc79cf7f9d426ca9fd0b9f
Jason Livesay <runvnc@gmail.com>
2023-12-11 06:33:32 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,33 @@ use llama_cpp_rs::{
     options::{ModelOptions, PredictOptions},
     LLama,
 };
+use std::env;
+use std::io::{self, Write};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut layers = 1000;
+    if args.len() > 2 {
+        layers = args[2].parse::<i32>().unwrap().into()
+    }
+
     let model_options = ModelOptions {
-        n_gpu_layers: 1000,
+        n_gpu_layers: layers,
         ..Default::default()
     };
-
-    let llama = LLama::new("models/starling-lm.gguf".into(), &model_options).unwrap();
+    let model_file = args[1].clone();
+    let llama = LLama::new(model_file, &model_options).unwrap();
 
     let predict_options = PredictOptions {
         tokens: 0,
-        threads: 10,
+        threads: 40,
         temperature: 0.001,
         top_k: 90,
         top_p: 0.86,
         token_callback: Some(Box::new(|token| {
-            println!("token1: {}", token);
-
+            print!("{}", token);
+            io::stdout().flush().unwrap();
             true
         })),
         ..Default::default()
