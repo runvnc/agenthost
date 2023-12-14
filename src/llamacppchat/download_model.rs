592cada5f7df09d545bfee51a9190298eefc95b9
Jason Livesay <runvnc@gmail.com>
2023-12-14 02:58:36 -0600

    update

--- src/llamacppchat/download_model.rs
+++ src/llamacppchat/download_model.rs
@@ -4,11 +4,12 @@ use std::io::{self, Write};
 
 pub async fn download_model_if_not_exists(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     if !Path::new(file_path).exists() {
-        println!("File does not exist. Downloading from {}", url);
+        println!("File does not exist. Downloading from {} to {}", url, file_path);
         
     let mut child = Command::new("wget")
         .arg(url)
-        .arg(format!("-O {}", file_path))
+        .arg("-O")
+        .arg(file_path)
         .stdout(Stdio::inherit()) // Inherit the stdout to display in console
         .stderr(Stdio::inherit()) // Inherit the stderr to display in console
         .spawn()
