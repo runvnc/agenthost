1238432e6d00dbae8d3b801e538ae584c4564ed8
Jason Livesay <runvnc@gmail.com>
2023-12-20 14:15:02 -0600

    aider: Add check_for_code and extract_code functions to extract_code.rs.
    # Aider chat conversation:
    
    ASSISTANT: To implement this, we need to modify `src/llamacppchat/extract_code.rs`:
    
    1. Add the function `check_for_code` that takes a string as input and returns a boolean indicating whether a code block exists in the string.
    2. Add the function `extract_code` that takes a string as input and returns the code block from the string.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/llamacppchat/extract_code.rs
    ```rust
    <<<<<<< SEARCH
                    // `HOST rollDice(numSides=4, count=2); `
    =======
    use regex::Regex;
    
    pub fn check_for_code(s: &str) -> bool {
        let re = Regex::new(r"`HOST .*; `").unwrap();
        re.is_match(s)
    }
    
    pub fn extract_code(s: &str) -> Option<&str> {
        let re = Regex::new(r"`HOST (.*); `").unwrap();
        re.captures(s).and_then(|cap| cap.get(1).map(|m| m.as_str()))
    }
    >>>>>>> REPLACE
    ```

--- src/llamacppchat/extract_code.rs
+++ src/llamacppchat/extract_code.rs
@@ -1,3 +1,13 @@
-                // `HOST rollDice(numSides=4, count=2); `
+use regex::Regex;
+
+pub fn check_for_code(s: &str) -> bool {
+    let re = Regex::new(r"`HOST .*; `").unwrap();
+    re.is_match(s)
+}
+
+pub fn extract_code(s: &str) -> Option<&str> {
+    let re = Regex::new(r"`HOST (.*); `").unwrap();
+    re.captures(s).and_then(|cap| cap.get(1).map(|m| m.as_str()))
+}
 
 
