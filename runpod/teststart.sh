165ba83a521c7f998ca3ff0e959f10ef85f3aeb3
Jason Livesay <runvnc@gmail.com>
2023-12-21 11:30:38 -0600

    aider: Store the output of the curl command in a variable and check if it contains the string "Agent Chat".
    # Aider chat conversation:
    
    ASSISTANT: To make this change we need to modify `runpod/teststart.sh` to:
    
    1. Store the output of the curl command in a variable.
    2. Check if the output contains the string "Agent Chat".
    
    Here is the *SEARCH/REPLACE* block:
    
    runpod/teststart.sh
    ```bash
    <<<<<<< SEARCH
        if curl https://r69krutxgj2l8i-3133.proxy.runpod.net/k; then
    =======
        response=$(curl -s https://r69krutxgj2l8i-3133.proxy.runpod.net/k)
        if [[ $response == *"Agent Chat"* ]]; then
    >>>>>>> REPLACE
    ```
    
    This change will store the output of the curl command in the variable `response`. It then checks if the string "Agent Chat" is in `response`. If it is, the script continues as before. If not, it sleeps for 1 second and then tries again.

--- runpod/teststart.sh
+++ runpod/teststart.sh
@@ -3,7 +3,8 @@
 start_time=$(date +%s)
 
 while true; do
-    if curl https://r69krutxgj2l8i-3133.proxy.runpod.net/k; then
+    response=$(curl -s https://r69krutxgj2l8i-3133.proxy.runpod.net/k)
+    if [[ $response == *"Agent Chat"* ]]; then
         end_time=$(date +%s)
         elapsed_time=$((end_time - start_time))
         echo "Time taken: $elapsed_time seconds"
