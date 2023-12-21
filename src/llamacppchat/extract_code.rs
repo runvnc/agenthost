9d36d7e8130a462fbb524f59625db037c23c25c1
Jason Livesay <runvnc@gmail.com>
2023-12-20 20:50:51 -0600

    aider: Fix regular expressions to not expect a semicolon at the end of the code snippet.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     cargo test --release
    
    And got this output:
    
       Compiling agenthost v0.1.0 (/files/agenthost)
        Finished release [optimized] target(s) in 1.24s
         Running unittests src/main.rs (target/release/deps/agenthost-7467366e4cc9ea83)
    
    running 2 tests
    test llamacppchat::extract_code::tests::test_check_for_code ... FAILED
    test llamacppchat::extract_code::tests::test_extract_code ... FAILED
    
    failures:
    
    ---- llamacppchat::extract_code::tests::test_check_for_code stdout ----
    thread 'llamacppchat::extract_code::tests::test_check_for_code' panicked at src/llamacppchat/extract_code.rs:33:9:
    assertion `left == right` failed
      left: false
     right: true
    
    ---- llamacppchat::extract_code::tests::test_extract_code stdout ----
    thread 'llamacppchat::extract_code::tests::test_extract_code' panicked at src/llamacppchat/extract_code.rs:40:9:
    assertion `left == right` failed
      left: None
     right: Some("33+156")
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    
    failures:
        llamacppchat::extract_code::tests::test_check_for_code
        llamacppchat::extract_code::tests::test_extract_code
    
    test result: FAILED. 0 passed; 2 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    
    error: test failed, to rerun pass `--bin agenthost`
    
    ASSISTANT: It seems like the regular expressions in the `check_for_code` and `extract_code` functions are not correctly identifying the code snippets in the test strings. The regular expressions are currently looking for a string that starts with "`HOST ", ends with "; `", and has any characters in between. However, the test strings do not have a semicolon at the end of the code snippet.
    
    To fix this, we need to modify the regular expressions in the `check_for_code` and `extract_code` functions to not expect a semicolon at the end of the code snippet.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/llamacppchat/extract_code.rs
    ```rust
    <<<<<<< SEARCH
    let re = Regex::new(r"`HOST .*; `").unwrap();
    =======
    let re = Regex::new(r"`HOST .*`").unwrap();
    >>>>>>> REPLACE
    ```
    
    src/llamacppchat/extract_code.rs
    ```rust
    <<<<<<< SEARCH
    let re = Regex::new(r"`HOST (.*); `").unwrap();
    =======
    let re = Regex::new(r"`HOST (.*)`").unwrap();
    >>>>>>> REPLACE
    ```

--- src/llamacppchat/extract_code.rs
+++ src/llamacppchat/extract_code.rs
@@ -9,14 +9,14 @@ use regex::Regex;
 //   I will roll the dice now: `HOST rollDice(sides=20, num=4)`  ..
 
 pub fn check_for_code(s: &str) -> bool {
-    let re = Regex::new(r"`HOST .*; `").unwrap();
+    let re = Regex::new(r"`HOST .*`").unwrap();
     let found = re.is_match(s);
     //println!("check_for_code input = {:?}  result = {:?}", s, found);
     found
 }
 
 pub fn extract_code(s: &str) -> Option<&str> {
-    let re = Regex::new(r"`HOST (.*); `").unwrap();
+    let re = Regex::new(r"`HOST (.*)`").unwrap();
     let result = re.captures(s).and_then(|cap| cap.get(1).map(|m| m.as_str()));
 
     //println!("extract_code input = {:?}  result = {:?}", s, result);
