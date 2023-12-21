f30184f881602656f31c7fdb44df1d1560ae2384
Jason Livesay <runvnc@gmail.com>
2023-12-21 11:30:34 -0600

    Updated teststart.sh to use a different proxy URL.

--- runpod/teststart.sh
+++ runpod/teststart.sh
@@ -3,7 +3,7 @@
 start_time=$(date +%s)
 
 while true; do
-    if curl https://r69krutxgj2l8i-3132.proxy.runpod.net/; then
+    if curl https://r69krutxgj2l8i-3133.proxy.runpod.net/k; then
         end_time=$(date +%s)
         elapsed_time=$((end_time - start_time))
         echo "Time taken: $elapsed_time seconds"
