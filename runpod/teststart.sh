42adf56037ffd7bd2dcf534398a837c22b360db1
Jason Livesay <runvnc@gmail.com>
2023-12-21 11:27:15 -0600

    aider: Add loop to continue executing curl command until success and record start time and calculate elapsed time.
    # Aider chat conversation:
    
    ASSISTANT: To make this change we need to modify `runpod/teststart.sh` to:
    
    1. Add a loop that will continue to execute the curl command until it succeeds.
    2. Record the start time before the loop and calculate the elapsed time once the curl command succeeds.
    
    Here is the *SEARCH/REPLACE* block:
    
    runpod/teststart.sh
    ```bash
    <<<<<<< SEARCH
    #!/bin/bash
    curl https://r69krutxgj2l8i-3132.proxy.runpod.net/
    =======
    #!/bin/bash
    
    start_time=$(date +%s)
    
    while true; do
        if curl https://r69krutxgj2l8i-3132.proxy.runpod.net/; then
            end_time=$(date +%s)
            elapsed_time=$((end_time - start_time))
            echo "Time taken: $elapsed_time seconds"
            break
        else
            sleep 1
        fi
    done
    >>>>>>> REPLACE
    ```

--- runpod/teststart.sh
+++ runpod/teststart.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-curl https://r69krutxgj2l8i-3132.proxy.runpod.net/
+
+start_time=$(date +%s)
+
+while true; do
+    if curl https://r69krutxgj2l8i-3132.proxy.runpod.net/; then
+        end_time=$(date +%s)
+        elapsed_time=$((end_time - start_time))
+        echo "Time taken: $elapsed_time seconds"
+        break
+    else
+        sleep 1
+    fi
+done
