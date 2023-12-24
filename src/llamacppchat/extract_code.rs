4f9e87692d086e63b73556ed532585155d924ea6
Jason Livesay <runvnc@gmail.com>
2023-12-23 23:46:03 -0600

    update

--- src/llamacppchat/extract_code.rs
+++ src/llamacppchat/extract_code.rs
@@ -1,11 +1,11 @@
 use regex::Regex;
 pub fn check_for_code(s: &str) -> bool {
-    let re = Regex::new(r"```Rhai_host\n.*?\n```").unwrap();
+    let re = Regex::new(r"```Rhai_host\n.*?```").unwrap();
     re.is_match(s)
 }
 
 pub fn extract_code(s: &str) -> Option<&str> {
-    let re = Regex::new(r"```Rhai_host\n(.*?)\n```").unwrap();
+    let re = Regex::new(r"```Rhai_host\n(.*?)```").unwrap();
     re.captures(s).and_then(|cap| cap.get(1).map(|m| m.as_str()))
 }
 /*
